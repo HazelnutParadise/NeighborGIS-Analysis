91fdcd601a002f06ef7deb2d8c0665714dee71c9
TimLai666 <tim930102@icloud.com>
2025-04-25 19:09:12 +0800

    調整主程式結構，將測試地址與批次轉換邏輯移至主函式內

--- geocoding.py
+++ geocoding.py
@@ -5,15 +5,6 @@ import time
 # 初始化匿名 ArcGIS geocoder
 geolocator = ArcGIS(timeout=10)
 
-# 測試地址
-df = pd.DataFrame({
-    'id': ['A', 'B'],
-    'address': [
-        'No. 101號, Section 2, Zhongcheng Rd, Shilin District, Taipei City, 11153',
-        '臺北市中正區愛國西路1號'
-    ]
-})
-
 # 查詢函式
 def arcgis_geocode(addr) -> pd.Series:
     try:
@@ -24,7 +15,17 @@ def arcgis_geocode(addr) -> pd.Series:
     except:
         return pd.Series({'latitude': None, 'longitude': None})
 
-# 批次轉換
-df[['latitude', 'longitude']] = df['address'].apply(arcgis_geocode)
+if __name__ == "__main__":
+    # 測試地址
+    df = pd.DataFrame({
+        'id': ['A', 'B'],
+        'address': [
+            'No. 101號, Section 2, Zhongcheng Rd, Shilin District, Taipei City, 11153',
+            '臺北市中正區愛國西路1號'
+        ]
+    })
+    
+    # 批次轉換
+    df[['latitude', 'longitude']] = df['address'].apply(arcgis_geocode)
 
-print(df)
+    print(df)
