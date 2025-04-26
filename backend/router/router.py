abd4d383d600c72a1d6b92759987db4fe38ab209
TimLai666 <tim930102@icloud.com>
2025-04-26 20:07:11 +0800

    新增靜態檔案路由並建立樣式表，改善前端樣式與結構

--- backend/router/router.py
+++ backend/router/router.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+
 from router.api import _set_api_routes
 from router.page import _set_page_routes
 
@@ -7,5 +10,8 @@ def set_routes(app: FastAPI) -> None:
     """
     Set up the routes for the FastAPI application.
     """
+    app.mount(
+        "/assets", StaticFiles(directory=os.path.join("frontend", "assets"))
+    )
     _set_api_routes(app)
     _set_page_routes(app)
