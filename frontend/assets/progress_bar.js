e366654856a8a437d17f79394b13c0d54625e810
TimLai666 <tim930102@icloud.com>
2025-05-02 11:07:40 +0800

    調整進度條顯示邏輯，新增對 loading 狀態的檢查，確保進度條正確顯示與隱藏

--- frontend/assets/progress_bar.js
+++ frontend/assets/progress_bar.js
@@ -1,16 +1,22 @@
 const ProgressBar=()=> {
+    let call_idx = 0;
     let timeCount = 0;
     let timeCounter = null;
     const PROGRESS_BAR = document.querySelector('.progress-bar');
     // 進度條控制函數
     function show() {
-        if (PROGRESS_BAR.classList.contains('loading')) return;
-        PROGRESS_BAR.classList.add('loading');
+        call_idx += 1;
         timeCounter = setInterval(() => {
             timeCount += 1; // 以 1 毫秒為單位計時
         }, 1);
     }
     function hide() {
+        if (!PROGRESS_BAR.classList.contains('loading')) return;
+        if (call_idx > 1) {
+            call_idx -= 1;
+            return;
+        }
+        
         clearInterval(timeCounter);
         
         const currentSeconds = Math.floor(timeCount / 1000);
